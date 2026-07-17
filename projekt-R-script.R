5e4e3d2c4935851c52a634ad16b7b8ef25630ed3
Alfred Backlund <alfred.backlund@aalto.fi>
2026-07-17 16:24:27 +0300

    Update 17.07.2026

--- projekt-R-script.R
+++ projekt-R-script.R
@@ -4,6 +4,10 @@
 setwd("C:/R-statistics-analysis")
 
 library(ggplot2)
+library(ggpattern)
+library(dplyr)
+library(deSolve)
+library(reshape2)
 
 ##############################
 
@@ -178,7 +182,6 @@ for (B in B_vals) {
   )
 }
 
-library(dplyr)
 
 stats <- df %>%
   group_by(experiments) %>%
@@ -233,7 +236,7 @@ ggplot(df, aes(x = value)) +
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     axis.line = element_line(color = "black"),
-    strip.text = element_text(size = 10, margin = margin(t = 0, b = 0)),
+    strip.text = element_text(linewidth = 10, margin = margin(t = 0, b = 0)),
     panel.spacing.y = unit(2, "lines")  # Increase vertical space between rows
   )
 ##############################
@@ -450,9 +453,6 @@ grid()
 
 # Reaktionshastighet 1
 
-# install.packages("deSolve")
-
-library(deSolve)
 
 # Definiera reaktionsparametrar
 k_plus <- 2.0    # Hastighetskonstant för framåtreaktionen
@@ -490,10 +490,6 @@ out <- ode(y = state_init, times = times, func = reaction_ode, parms = parameter
 # Konvertera till data.frame för enklare hantering
 out_df <- as.data.frame(out)
 
-# Rita kurvor för koncentrationerna
-library(ggplot2)
-
-library(reshape2)
 out_melt <- melt(out_df, id = "time")
 
 ggplot(out_melt, aes(x = time, y = value, color = variable)) +
@@ -771,8 +767,6 @@ ggplot(df, aes(x = x, y = density, fill = distribution)) +
 
 # Inferens 5
 
-library(ggpattern) # for dashed alpha region area
-
 x <- seq(-3, 7, length.out = 1000)
 
 df <- data.frame(
@@ -883,8 +877,6 @@ ggplot(df, aes(x = T_C, y = prob_factor)) +
 ##############################
 
 # Histogram 1
-
-library(ggplot2)
 
 # Generate random data
 x <- rnorm(n = 100, mean = 0, sd = 1)
@@ -2335,4 +2327,551 @@ ggplot(df, aes(x = x, y = density, fill = distribution)) +
     linetype = "dashed",
     linewidth = 1.2
   )
+##############################
+
+
+
+
+##############################
+
+# P(B>A) #1
+
+x <- seq(-5, 7, length.out = 100)
+
+df <- data.frame(
+  x = rep(x, 2),
+  density = c(
+    dnorm(x, mean = 0, sd = 1),
+    dnorm(x, mean = 2, sd = 1)
+  ),
+  distribution = factor(rep(c("Mean 0", "Mean 2"), each = length(x)))
+)
+
+ggplot(df, aes(x = x, y = density, fill = distribution)) +
+  geom_line(linewidth = 1.2) +
+  scale_x_continuous(
+    breaks = c(0, 2),
+    labels = c(
+      expression(mu[A] == 0),
+      expression(mu[B] == 2)
+    )
+  ) +
+  labs(
+    title = NULL,
+    x = NULL,
+    y = NULL,
+    color = "Distribution"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 14)
+  ) +
+  geom_segment(aes(x = 0, xend = 0, y = 0, yend = max(df$density)), color = "red", linetype = "dashed", linewidth = 1) +
+  geom_segment(aes(x = 2, xend = 2, y = 0, yend = max(df$density)), color = "red", linetype = "dashed", linewidth = 1)
+##############################
+
+
+
+##############################
+
+# P(B>A) #2
+
+mu_diff <- 2 - 0
+sd_diff <- sqrt(1^2 + 1^2)
+
+x <- seq(-3, 7, length.out = 400)
+density <- dnorm(x, mean = mu_diff, sd = sd_diff)
+
+df_diff <- data.frame(x = x, density = density)
+
+df_shade <- subset(df_diff, x >= 0)
+p_superiority <- 1 - pnorm(0, mean = mu_diff, sd = sd_diff)
+
+ggplot(df_diff, aes(x = x, y = density)) +
+  geom_line(linewidth = 1.2, color = "black") +
+  
+  geom_ribbon(
+    data = df_shade,
+    aes(ymin = 0, ymax = density),
+    fill = "orange",
+    alpha = 0.35
+  ) +
+  
+  # Mean difference line (red)
+  geom_segment(
+    aes(
+      x = mu_diff, xend = mu_diff,
+      y = 0, yend = dnorm(mu_diff, mu_diff, sd_diff)
+    ),
+    color = "red", linetype = "dashed", linewidth = 1.2
+  ) +
+  
+  # Zero reference line (grey)
+  geom_segment(
+    aes(
+      x = 0, xend = 0,
+      y = 0, yend = dnorm(0, mu_diff, sd_diff)
+    ),
+    color = "grey40", linetype = "dashed", linewidth = 1.2
+  ) +
+  
+  annotate(
+    "text",
+    x = 5, y = max(df_diff$density) * 0.85,
+    label = paste0("P(B > A) \u2248 ", sprintf("%.2f", p_superiority)),
+    size = 6,
+    color = "orange"
+  ) +
+  
+  scale_x_continuous(
+    breaks = c(0, mu_diff),
+    labels = c(
+      expression(0),
+      expression(mu[delta] == 2)
+    )
+  ) +
+  
+  labs(
+    title = NULL,
+    x = NULL,
+    y = NULL
+  ) +
+  
+  theme_minimal() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 14)
+  )
+##############################
+
+
+
+##############################
+
+# SD vs SE exempel 1
+
+# Parameters
+mu <- 100
+sigma <- 20
+sample_sizes <- c(10, 20, 50, 100)
+
+# Colors
+cols <- c(
+  "10" = "brown",
+  "20" = "blue",
+  "50" = "green",
+  "100" = "red"
+)
+
+# X-axis values
+x <- seq(80, 120, length.out = 1000)
+
+# Create plotting data
+plot_data <- data.frame()
+
+for (n in sample_sizes) {
+  
+  se <- sigma / sqrt(n)
+  
+  plot_data <- rbind(
+    plot_data,
+    data.frame(
+      x = x,
+      density = dnorm(x, mean = mu, sd = se),
+      n = factor(n)
+    )
+  )
+}
+
+# Plot
+ggplot(plot_data, aes(x = x, y = density, colour = n)) +
+  geom_line(linewidth = 1.2) +
+  geom_vline(xintercept = mu,
+             linetype = "dashed",
+             colour = "black",
+             linewidth = 0.8) +
+  scale_color_manual(values = cols, name = "Stickprovsstorlek (n)") +
+  labs(
+    title = NULL,
+    x = "Stickprovsmedelvärde",
+    y = NULL
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 14)
+  )
+##############################
+
+
+
+##############################
+
+# SD vs SE exempel 2
+
+# Parameters
+mu_A <- 100
+mu_B <- 110
+sigma <- 20
+n <- 20
+se <- sigma / sqrt(n)
+
+# 95% confidence limits
+A_lower <- mu_A - 1.96 * se
+A_upper <- mu_A + 1.96 * se
+
+B_lower <- mu_B - 1.96 * se
+B_upper <- mu_B + 1.96 * se
+
+shade_A = x >= A_lower & x <= A_upper
+shade_B = x >= B_lower & x <= B_upper
+
+
+# X values
+x <- seq(80, 130, length.out = 1000)
+
+df <- data.frame(
+  x = rep(x, 2),
+  density = c(
+    dnorm(x, mean = mu_A, sd = se),
+    dnorm(x, mean = mu_B, sd = se)
+  ),
+  distribution = factor(rep(c("Grupp A", "Grupp B"), each = length(x))),
+  shade = c(
+    x >= A_lower & x <= A_upper,
+    x >= B_lower & x <= B_upper
+  )
+)
+
+ggplot(df, aes(x = x, y = density, fill = distribution)) +
+  geom_line(linewidth = 1.2) +
+  scale_x_continuous(
+    breaks = c(mu_A, mu_B),
+    labels = c(
+      expression(mu[A] == 100),
+      expression(mu[B] == 110)
+    )
+  ) +
+  
+  geom_ribbon(
+    data = subset(df, shade),
+    aes(ymin = 0, ymax = density, fill = distribution),
+    alpha = 0.3,
+    colour = NA
+  ) +
+  scale_fill_manual(
+    values = c("Grupp A" = "red",
+               "Grupp B" = "blue"),
+    name = expression(paste("95% CI (n = 20)"))
+  ) +
+  
+  labs(
+    title = NULL,
+    x = NULL,
+    y = NULL,
+    color = "Distribution"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 14)
+  ) +
+  geom_segment(aes(x = mu_A, xend = mu_A, y = 0, yend = max(df$density)), color = "red", linetype = "dashed", linewidth = 1) +
+  geom_segment(aes(x = mu_B, xend = mu_B, y = 0, yend = max(df$density)), color = "blue", linetype = "dashed", linewidth = 1)
+##############################
+
+
+
+##############################
+
+# SD vs SE exempel 3
+
+x <- seq(80, 130, length.out = 1000)
+
+
+# Parameters
+mu_A <- 100
+mu_B <- 110
+sigma <- 20
+n <- 20
+se <- sigma / sqrt(n)
+
+# 95% confidence limits
+A_lower <- mu_A - 1.96 * se
+A_upper <- mu_A + 1.96 * se
+
+B_lower <- mu_B - 1.96 * se
+B_upper <- mu_B + 1.96 * se
+
+shade_A = x >= A_lower & x <= A_upper
+shade_B = x >= B_lower & x <= B_upper
+
+
+df <- data.frame(
+  x = rep(x, 2),
+  density = c(
+    dnorm(x, mean = mu_A, sd = se),
+    dnorm(x, mean = mu_B, sd = se)
+  ),
+  distribution = factor(rep(c("Grupp A", "Grupp B"), each = length(x))),
+  shade = c(
+    x >= A_lower & x <= A_upper,
+    x >= B_lower & x <= B_upper
+  )
+)
+
+
+crit_val <- A_upper
+shade_alpha <- subset(df, distribution == "Grupp A" & x >= crit_val)
+
+alt_density_at_crit <- dnorm(
+  crit_val,
+  mean = mu_B,
+  sd = se
+)
+
+ggplot(df, aes(x = x, y = density, fill = distribution)) +
+  geom_ribbon(
+    data = subset(df, x >= crit_val & distribution == "Grupp B"),
+    aes(x = x, ymin = 0, ymax = density),
+    alpha = 0.5,
+    inherit.aes = FALSE,
+    fill = "orange"
+  ) +
+  geom_ribbon_pattern(
+    data = subset(df, distribution == "Grupp A" & x >= crit_val),
+    aes(x = x, ymin = 0, ymax = density),
+    fill = "NA",
+    pattern = "stripe",
+    pattern_fill = "brown",
+    pattern_angle = 45,
+    pattern_density = 0.1,
+    inherit.aes = FALSE
+  ) +
+  geom_line(linewidth = 1.2) +
+  scale_x_continuous(
+    breaks = c(mu_A, mu_B),
+    labels = c(
+      expression(mu[A]==100),
+      expression(mu[B]==110)
+    )
+  ) +
+  labs(
+    title = NULL,
+    x = NULL,
+    y = NULL,
+    color = "Distribution"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 14)
+  ) +
+  geom_segment(aes(x = mu_A, xend = mu_A, y = 0, yend = max(df$density)), color = "red", linetype = 1, linewidth = 1) +
+  geom_segment(aes(x = mu_B, xend = mu_B, y = 0, yend = max(df$density)), color = "blue", linetype = 1, linewidth = 1) +
+  annotate("text", x = 119, y = 0.06, label = expression(1 - beta %~~% 0.61), color = "orange", size = 5) +
+  
+  geom_segment(aes(x = crit_val, xend = crit_val, y = 0, yend = alt_density_at_crit),
+               color = "brown", linetype = "dashed", linewidth = 1)
+##############################
+
+
+
+##############################
+
+# SD vs SE exempel 4
+
+x <- seq(80, 130, length.out = 1000)
+
+
+# Parameters
+mu_A <- 100
+mu_B <- 110
+sigma <- 20
+n <- 35
+se <- sigma / sqrt(n)
+
+# 95% confidence limits
+A_lower <- mu_A - 1.96 * se
+A_upper <- mu_A + 1.96 * se
+
+B_lower <- mu_B - 1.96 * se
+B_upper <- mu_B + 1.96 * se
+
+shade_A = x >= A_lower & x <= A_upper
+shade_B = x >= B_lower & x <= B_upper
+
+
+df <- data.frame(
+  x = rep(x, 2),
+  density = c(
+    dnorm(x, mean = mu_A, sd = se),
+    dnorm(x, mean = mu_B, sd = se)
+  ),
+  distribution = factor(rep(c("Grupp A", "Grupp B"), each = length(x))),
+  shade = c(
+    x >= A_lower & x <= A_upper,
+    x >= B_lower & x <= B_upper
+  )
+)
+
+
+crit_val <- A_upper
+shade_alpha <- subset(df, distribution == "Grupp A" & x >= crit_val)
+
+alt_density_at_crit <- dnorm(
+  crit_val,
+  mean = mu_B,
+  sd = se
+)
+
+ggplot(df, aes(x = x, y = density, fill = distribution)) +
+  geom_ribbon(
+    data = subset(df, x >= crit_val & distribution == "Grupp B"),
+    aes(x = x, ymin = 0, ymax = density),
+    alpha = 0.5,
+    inherit.aes = FALSE,
+    fill = "orange"
+  ) +
+  geom_ribbon_pattern(
+    data = subset(df, distribution == "Grupp A" & x >= crit_val),
+    aes(x = x, ymin = 0, ymax = density),
+    fill = "NA",
+    pattern = "stripe",
+    pattern_fill = "brown",
+    pattern_angle = 45,
+    pattern_density = 0.1,
+    inherit.aes = FALSE
+  ) +
+  geom_line(linewidth = 1.2) +
+  scale_x_continuous(
+    breaks = c(mu_A, mu_B),
+    labels = c(
+      expression(mu[A]==100),
+      expression(mu[B]==110)
+    )
+  ) +
+  labs(
+    title = NULL,
+    x = NULL,
+    y = NULL,
+    color = "Distribution"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 14)
+  ) +
+  geom_segment(aes(x = mu_A, xend = mu_A, y = 0, yend = max(df$density)), color = "red", linetype = 1, linewidth = 1) +
+  geom_segment(aes(x = mu_B, xend = mu_B, y = 0, yend = max(df$density)), color = "blue", linetype = 1, linewidth = 1) +
+  annotate("text", x = 119, y = 0.06, label = expression(1 - beta %~~% 0.84), color = "orange", size = 5) +
+  
+  geom_segment(aes(x = crit_val, xend = crit_val, y = 0, yend = alt_density_at_crit),
+               color = "brown", linetype = "dashed", linewidth = 1)
+##############################
+
+
+
+##############################
+
+# SD vs SE exempel 5
+
+# Parameters
+mu_A <- 100
+mu_B <- 110
+sigma <- 20
+n <- 20
+se <- sigma / sqrt(n)
+
+# X values
+x <- seq(35, 175, length.out = 1000)
+
+summary_df <- data.frame(
+  group = c("Grupp A", "Grupp B", "Grupp A", "Grupp B"),
+  type  = c("sample", "sample", "mean", "mean"),
+  mean  = c(mu_A, mu_B, mu_A, mu_B),
+  sd    = c(sigma, sigma, se, se)
+)
+
+summary_df$type <- factor(
+  summary_df$type,
+  levels = c("sample", "mean")
+)
+
+df_dist <- do.call(rbind, lapply(1:nrow(summary_df), function(i) {
+  
+  dens <- dnorm(
+    x,
+    mean = summary_df$mean[i],
+    sd   = summary_df$sd[i]
+  )
+  
+  data.frame(
+    x = x,
+    density = dens / max(dens),
+    group = summary_df$group[i],
+    type = summary_df$type[i]
+  )
+}))
+
+
+ggplot(df_dist, aes(x = x, y = density, color = group, fill = group)) +
+  geom_line(linewidth = 1.2) +
+  geom_area(alpha = 0.2, position = "identity") +
+  
+  facet_wrap(~ type, nrow = 1,
+             labeller = labeller(
+               type = c(
+                 "sample"   = "Stickprov (SD)",
+                 "mean" = "Medelvärden (SE)"
+               )
+             )) +
+  
+  labs(
+    x = NULL,
+    y = NULL
+  ) +
+  
+  theme_minimal(base_size = 14) +
+  theme(
+    legend.position = "top",
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text.y = element_blank(),
+    axis.line = element_line(color = "black"),
+    axis.text = element_text(size = 12)
+  ) + 
+  scale_color_manual(
+    name = NULL,
+    values = c("Grupp A" = "red", "Grupp B" = "blue")
+  ) +
+  scale_fill_manual(
+    name = NULL,
+    values = c("Grupp A" = "red", "Grupp B" = "blue")
+  ) +
+  geom_segment(aes(x = mu_A, xend = mu_A, y = 0, yend = max(df_dist$density)), color = "red", linetype = 2, linewidth = 1) +
+  geom_segment(aes(x = mu_B, xend = mu_B, y = 0, yend = max(df_dist$density)), color = "blue", linetype = 2, linewidth = 1)
 ##############################
